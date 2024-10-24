c489e7f907ce1a51bb4791086a614abafcac248c
Braian <bryansalles45@gmail.com>
2024-10-24 08:42:05 -0300

    refactor: refatoring app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
-
 class HelloWorld(Resource):
     
     def get(self):
@@ -20,16 +18,10 @@ class Users(Resource):
     def get(self):
         return self.dic
     
-
-
-
-
-
     
+
 api.add_resource(HelloWorld, '/')
 api.add_resource(Users, '/users')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
